2344597621bc128af43e78b87e2e57800f1933a0
jannay11jr <jannaycaselles13@gmail.com>
2024-09-20 11:37:44 +0200

    Page book working

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -10,19 +10,33 @@ import { RoomsService } from '../rooms.service';
 })
 export class BookComponent implements OnInit {
 
+  name_of_rooms: any[] = [];
+  selectedRoom: string = '';
+  startDate: string = '';
+  endDate: string = '';
+  whatsappPhoneNumber: string = '677777777';  // Número de teléfono para WhatsApp
+
   constructor(private roomsinfo: RoomsService) {}
 
-  name_of_rooms!: any [];
-
-  ngOnInit(): void{
+  ngOnInit(): void {
     this.getRooms();
   }
 
-    getRooms(): void {
+  getRooms(): void {
     this.roomsinfo.getRooms()
-    .subscribe(resp => {
-      this.name_of_rooms = resp.rooms;
-      console.log(resp.rooms);
-    })
+      .subscribe(resp => {
+        this.name_of_rooms = resp.rooms;
+      });
+  }
+
+  sendWhatsAppMessage(): void {
+    if (this.selectedRoom && this.startDate && this.endDate) {
+      const message = `Buenos días, me gustaría reservar la habitación ${this.selectedRoom} para las fechas del ${this.startDate} al ${this.endDate}. ¿Me podría decir si está disponible y de qué forma podemos gestionarlo?`;
+      const whatsappURL = `https://wa.me/${this.whatsappPhoneNumber}?text=${encodeURIComponent(message)}`;
+
+      window.open(whatsappURL, '_blank');
+    } else {
+      alert('Por favor, seleccione una habitación y un rango de fechas.');
+    }
   }
 }
